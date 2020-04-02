598ebfacded450399bbf6fdf29788bb37baa54da
W3Edd <edsonmanuelcarballovera@gmail.com>
2020-04-01 19:38:15 -0600

    Clase DAOProfesor creada

--- Prueba.java
+++ Prueba.java
@@ -1,5 +1,14 @@
+import DAO.DAOAdministrador;
+import tools.P;
+
 public class Prueba {
 	public static void main(String[] args) {
-
+		DAOAdministrador admin = new DAOAdministrador(
+				"Edson Manuel",
+				"Carballo Vera",
+				"edsonn1999@hotmail.com",
+				"relojito"
+		);
+		P.p(admin.signUp());
 	}
 }
