320c68802af2e91d20d324324d8dfbed1d439b97
W3Edd <edsonmanuelcarballovera@gmail.com>
2020-04-14 21:24:21 -0500

    Added multiple exception codes to CustomException

--- Exceptions/CustomException.java
+++ Exceptions/CustomException.java
@@ -1,8 +1,11 @@
 package Exceptions;
 
+import tools.Array;
+
 public class CustomException extends Exception {
 	private String causeMessage;
 	private String exceptionCode;
+	private Array codes = new Array();
 	
 	public CustomException() {
 		super("Custom Exception");
@@ -11,6 +14,18 @@ public class CustomException extends Exception {
 	public CustomException(String exceptionMessage) {
 		super(exceptionMessage);
 		this.causeMessage = exceptionMessage;
+	}
+	
+	public CustomException(String exceptionMessage, String exceptionCode) {
+		super(exceptionMessage);
+		this.causeMessage = exceptionMessage;
+		this.exceptionCode = exceptionCode;
+	}
+	
+	public CustomException(String exceptionMessage, String... exceptionCodes) {
+		super(exceptionMessage);
+		this.causeMessage = exceptionMessage;
+		this.addCodes(exceptionCodes);
 	}
 	
 	public String getCauseMessage() {
@@ -27,5 +42,23 @@ public class CustomException extends Exception {
 	
 	public void setExceptionCode(String exceptionCode) {
 		this.exceptionCode = exceptionCode;
+	}
+	
+	public void addCode(String code) {
+		this.codes.add(code);
+	}
+	
+	public void addCodes(String... exceptionCodes) {
+		for (String code: exceptionCodes) {
+			this.addCode(code);
+		}
+	}
+	
+	public String[] getCodes() {
+		String[] returnCodes = new String[this.codes.len()];
+		for (int i = 0; i < this.codes.len(); i++) {
+			returnCodes[i] = this.codes.getString(i);
+		}
+		return returnCodes;
 	}
 }
