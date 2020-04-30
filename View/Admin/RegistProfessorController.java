65f87e2256653bf8af5adedbb38ab9c9b2a8fd87
VeggieCode <alexisao@hotmail.com>
2020-04-30 01:07:21 -0500

    Creación de la ventana Registrar Profesor y su correspondiente controlador

---
+++ View/Admin/RegistProfessorController.java
@@ -0,0 +1,16 @@
+package View.Admin;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class RegistProfessorController extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
