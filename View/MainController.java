598f289d784fabbbe0c7de8564f9a78c670e821f
W3Edd <edsonmanuelcarballovera@gmail.com>
2020-04-29 22:18:36 -0500

    MainController modificado

--- View/MainController.java
+++ View/MainController.java
@@ -21,7 +21,7 @@ public class MainController extends Application {
 				)
 			);
 		} catch (IOException e) {
-			new Logger().log(e);
+			new Logger().log(e, false);
 		}
 	}
 	
@@ -43,7 +43,7 @@ public class MainController extends Application {
 			);
 			MainController.stage.show();
 		} catch (IOException e) {
-			new Logger().log(e,false);
+			new Logger().log(e, false);
 		}
 	}
 }
