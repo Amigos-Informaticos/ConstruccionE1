94e7dfde5dc44db945831aa2cde1bf1f4c0a1018
EfrainCami <zs18012138@estudiantes.uv.mx>
2020-04-29 21:28:30 -0500

    Haciendo el login

--- View/Login/LoginController.java
+++ View/Login/LoginController.java
@@ -1,4 +1,19 @@
 package View.Login;
 
-public class LoginController {
+import View.MainController;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void onIngresarButtonClick(){
+        MainController mainController = new MainController();
+        mainController.activate("Registrar Estudiante","RegistAStudent/RegistAStudent.fxml");
+    }
 }
