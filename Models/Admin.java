7ccd9fedcb9faeadfc214a1db4fb4e92abdcad41
VeggieCode <alexisao@hotmail.com>
2020-05-01 16:20:47 -0500

    Implementación del método filltableProfessor

--- Models/Admin.java
+++ Models/Admin.java
@@ -6,18 +6,7 @@ import javafx.collections.ObservableList;
 public class Admin {
 	public Admin(){
 	}
-	/*public Admin(String nombres, String apellidos, String correoElectronico, String contrasena) {
-		super(nombres, apellidos, correoElectronico, contrasena);
-	}*/
-	
-	/*public Admin(Admin admin) {
-		if (admin != null) {
-			this.setNames(admin.getNames());
-			this.setLastnames(admin.getLastnames());
-			this.setEmail(admin.getEmail());
-			this.setPassword(admin.getPassword());
-		}
-	}*/
+
 
 	public boolean fillTableProfessor(ObservableList<Professor> listProfessor){
 		boolean filled = false;
