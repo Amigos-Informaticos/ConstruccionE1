ee303ed20ed6af2b62c4389d0ed9d6485e57ccec
Edson Manuel Carballo Vera <33274873+W3Edd@users.noreply.github.com>
2020-05-03 21:29:20 -0500

    Eliminado método printArray

--- tools/P.java
+++ tools/P.java
@@ -108,12 +108,7 @@ public abstract class P {
 	public static void printArray(Array array) {
 		printArray(array, false);
 	}
-	
-	@Deprecated
-	public static void printArray(Array array, boolean splitLines) {
-		array.print(splitLines);
-	}
-	
+
 	public static void err(Object err) {
 		System.err.println(err.toString());
 	}
