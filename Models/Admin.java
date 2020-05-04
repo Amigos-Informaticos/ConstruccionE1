3fa6c4f2f1c3d4810106cee0f87e037c44f48118
VeggieCode <alexisao@hotmail.com>
2020-05-04 16:56:25 -0500

    Creación de la prueba AdminTests, método getHashedPassword con pruebas

--- Models/Admin.java
+++ Models/Admin.java
@@ -2,6 +2,11 @@ package Models;
 
 import DAO.DAOAdmin;
 import javafx.collections.ObservableList;
+import tools.Logger;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class Admin extends User {
 	public Admin() {
@@ -31,5 +36,24 @@ public class Admin extends User {
 	
 	public boolean isRegistered() {
 		return new DAOAdmin(this).isRegistered();
+	}
+
+
+	public String getHashedPassword(String password){
+		String hashedPassword = null;
+		try {
+			MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+			byte[] message = messageDigest.digest(password.getBytes());
+			BigInteger number = new BigInteger(1, message);
+			StringBuilder hashed = new StringBuilder(number.toString(16));
+			while (hashed.length() < 32) {
+				hashed.insert(0, "0");
+			}
+			hashedPassword = hashed.toString();
+
+		} catch (NoSuchAlgorithmException e) {
+			new Logger().log(e);
+		}
+		return hashedPassword;
 	}
 }
---
+++ Tests/AdminTests.java
@@ -0,0 +1,12 @@
+package Tests;
+
+import Models.Admin;
+import org.junit.Test;
+
+public class AdminTests {
+    @Test
+    public void a_getHashedPassword(){
+        Admin admin = new Admin();
+        System.out.println(admin.getHashedPassword("qRjs4951"));
+    }
+}
