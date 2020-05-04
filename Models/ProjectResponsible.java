d4b272b0218b48e61e4a9feb07cfb7a4c1f2c4ec
VeggieCode <alexisao@hotmail.com>
2020-05-04 17:31:08 -0500

    DAOProjectResponsible, ManageStudentController con fxml y ProjectResponsible -Autor Efraín Ramirez

---
+++ Models/ProjectResponsible.java
@@ -0,0 +1,40 @@
+package Models;
+
+public class ProjectResponsible {
+    private String email;
+    private String names;
+    private String lastNames;
+    
+    public ProjectResponsible() {
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public String getNames() {
+        return names;
+    }
+    
+    public void setNames(String names) {
+        this.names = names;
+    }
+    
+    public String getLastNames() {
+        return lastNames;
+    }
+    
+    public void setLastNames(String lastNames) {
+        this.lastNames = lastNames;
+    }
+    
+    public boolean isComplete() {
+        return this.email != null &&
+            this.names != null &&
+            this.lastNames != null;
+    }
+}
