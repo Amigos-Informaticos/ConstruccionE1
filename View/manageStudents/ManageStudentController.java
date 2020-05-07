fcdb61f92864a851f282cafcfd05c57044aed559
EfrainCami <zs18012138@estudiantes.uv.mx>
2020-05-06 20:29:56 -0500

    Avance 6/5/2020

--- View/manageStudents/ManageStudentController.java
+++ View/manageStudents/ManageStudentController.java
@@ -14,50 +14,42 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ManageStudentController implements Initializable {
-	@FXML
-	private TableView<Student> tblViewStudent;
-	@FXML
-	private TableColumn<Student, String> clmnName;
-	@FXML
-	private TableColumn<Student, String> clmnLastame;
-	@FXML
-	private TableColumn<Student, String> clmnRegno;
-	
-	
-	@FXML
-	JFXTextField txtName;
-	@FXML
-	JFXTextField txtLastname;
-	@FXML
-	JFXTextField txtRegNo;
-	@FXML
-	JFXTextField txtEmail;
-	
-	ObservableList<Student> listStudent;
-	
-	public void instanceStudent(Student student) {
-		student.setNames(txtName.getText());
-		student.setLastnames(txtLastname.getText());
-		student.setRegNumber(txtRegNo.getText());
-		student.setEmail(txtEmail.getText());
-	}
-	
-	public void cleanFormStudent() {
-		txtName.setText(null);
-		txtLastname.setText(null);
-		txtRegNo.setText(null);
-		txtEmail.setText(null);
-	}
-	
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		listStudent = FXCollections.observableArrayList();
-		new Student().fillTableStudent(listStudent);
-		tblViewStudent.setItems(listStudent);
-		clmnName.setCellValueFactory(new PropertyValueFactory<Student, String>("names"));
-		clmnLastame.setCellValueFactory(new PropertyValueFactory<Student, String>("lastnames"));
-		clmnRegno.setCellValueFactory(new PropertyValueFactory<Student, String>("regNumber"));
-		
-		
-	}
+    @FXML private TableView<Student> tblViewStudent;
+    @FXML private TableColumn<Student, String> clmnName;
+    @FXML private TableColumn<Student, String> clmnLastame;
+    @FXML private TableColumn<Student, String> clmnRegno;
+
+
+    @FXML JFXTextField txtName;
+    @FXML JFXTextField txtLastname;
+    @FXML JFXTextField txtRegNo;
+    @FXML JFXTextField txtEmail;
+
+    ObservableList<Student> listStudent;
+
+    public void instanceStudent(Student student){
+        student.setNames(txtName.getText());
+        student.setLastnames(txtLastname.getText());
+        student.setRegNumber(txtRegNo.getText());
+        student.setEmail(txtEmail.getText());
+    }
+
+    public void cleanFormStudent(){
+        txtName.setText(null);
+        txtLastname.setText(null);
+        txtRegNo.setText(null);
+        txtEmail.setText(null);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        listStudent = FXCollections.observableArrayList();
+        new Student().fillTableStudent(listStudent);
+        tblViewStudent.setItems(listStudent);
+        clmnName.setCellValueFactory(new PropertyValueFactory<Student,String>("names"));
+        clmnLastame.setCellValueFactory(new PropertyValueFactory<Student,String>("lastnames"));
+        clmnRegno.setCellValueFactory(new PropertyValueFactory<Student,String>("regNumber"));
+
+
+    }
 }
