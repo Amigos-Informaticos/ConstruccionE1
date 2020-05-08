5e64e9fed644988250104fbee0f60bf0ce2359cb
W3Edd <edsonmanuelcarballovera@gmail.com>
2020-05-08 16:34:45 -0500

    Version funcional de MainController.Sizes

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ import View.MainController;
 
 public class Main {
 	public static void main(String[] args) {
-		MainController.hit("CreateProject", "Pantalla De Login", "LARGE");
+		MainController.hit("CreateProject", "Pantalla De Login", MainController.Sizes.MID);
 	}
 }
