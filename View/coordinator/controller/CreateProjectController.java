c277eda2fb8e85e53fdc595309283383c533beb0
W3Edd <edsonmanuelcarballovera@gmail.com>
2020-07-03 13:26:23 -0500

    Commit antes de hacer pull

--- View/coordinator/controller/CreateProjectController.java
+++ View/coordinator/controller/CreateProjectController.java
@@ -1,13 +1,11 @@
 package View.coordinator.controller;
 
 import Models.CalendarizedActivity;
-import Models.Organization;
 import View.MainController;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXDatePicker;
 import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
-import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -19,43 +17,39 @@ import java.util.ResourceBundle;
 public class CreateProjectController implements Initializable {
 	@FXML private static JFXComboBox<String> cmbOrganizations;
 	
-	@FXML private static JFXTextField txtName;
-	@FXML private static JFXTextArea txtDescription;
-	@FXML private static JFXTextField txtGeneralObjective;
-	@FXML private static JFXTextField txtMediateObjective;
-	@FXML private static JFXTextField txtInmediateObjective;
-	@FXML private static JFXTextField txtMethodology;
-	@FXML private static JFXTextArea txtResources;
-	@FXML private static JFXTextArea txtResponsibilities;
-	@FXML private static JFXTextField txtCapacity;
-	@FXML private static JFXTextField txtPositionResponsible;
-	@FXML private static JFXTextField txtEmailResponsible;
-	@FXML private static JFXTextField txtNameResponsible;
-	@FXML private static JFXTextField txtLastnameResponsible;
-	@FXML private static JFXComboBox<String> cmbArea;
+	private static String nombre;
+	
+	@FXML private JFXTextField txtName;
+	@FXML private JFXTextArea txtDescription;
+	@FXML private JFXTextField txtGeneralObjective;
+	@FXML private JFXTextField txtMediateObjective;
+	@FXML private JFXTextField txtInmediateObjective;
+	@FXML private JFXTextField txtMethodology;
+	@FXML private JFXTextArea txtResources;
+	@FXML private JFXTextArea txtResponsibilities;
+	@FXML private JFXTextField txtCapacity;
+	@FXML private JFXTextField txtPositionResponsible;
+	@FXML private JFXTextField txtEmailResponsible;
+	@FXML private JFXTextField txtNameResponsible;
+	@FXML private JFXTextField txtLastnameResponsible;
+	@FXML private JFXComboBox<String> cmbArea;
 	
 	@FXML private JFXTextField[] txtNamesOfActivity = new JFXTextField[6];
 	@FXML private JFXTextField[] txtDatesOfActivity = new JFXTextField[6];
 	
-	@FXML private static JFXDatePicker initialDate;
-	@FXML private static JFXDatePicker finalDate;
+	@FXML private JFXDatePicker initialDate;
+	@FXML private JFXDatePicker finalDate;
 	
 	ObservableList<String> listOrganizations;
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		if(location.toString().substring(location.toString().lastIndexOf('/') + 1).equals("CreateProject.fxml")){
-			listOrganizations = FXCollections.observableArrayList();
-			new Organization().fillOrganizationNames(listOrganizations);
-			cmbOrganizations.setItems(listOrganizations);
-		}else{
-		
-		}
-		
+	
 	}
 	
 	@FXML
 	public void onClickCalendarization(MouseEvent clickEvent){
+		nombre = txtName.getText();
 		MainController.activate("ProjectCalendarization",
 			"Calendarizacion de Proyecto",
 			MainController.Sizes.MID);
@@ -79,6 +73,7 @@ public class CreateProjectController implements Initializable {
 	}
 	
 	public void onClickBack(MouseEvent clickEvent){
-		MainController.activate("CreateProjectController","Crear Proyecto", MainController.Sizes.LARGE);
+		txtName.setText(nombre);
+		MainController.activate("CreateProject","Crear Proyecto", MainController.Sizes.LARGE);
 	}
 }
