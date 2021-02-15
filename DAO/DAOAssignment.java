1a25343e275699e2265bb10d16068b5fd6c5465e
Edson Manuel Carballo Vera <edsonmanuelcarballovera@gmail.com>
2021-02-14 21:43:21 -0600

    Refactorizaciones en la clase DAOAssignment

--- DAO/DAOAssignment.java
+++ DAO/DAOAssignment.java
@@ -23,7 +23,7 @@ public class DAOAssignment {
 	}
 	
 	public boolean assignProject(Document document) {
-		boolean assigned;
+		boolean assigned = false;
 		assert this.assignment.isComplete() :
 			"Assignment is incomplete: DAOAssignment.assignProject()";
 		String studentId = new DAOStudent(this.assignment.getStudent()).getId();
@@ -31,22 +31,20 @@ public class DAOAssignment {
 		String professorId = new DAOProfessor(this.assignment.getProfessor()).getId();
 		String query = "SELECT COUNT(idPracticante) AS TOTAL FROM Asignacion " +
 			"WHERE idPracticante = ? AND idProyecto = ? AND estaActivo = 1";
-		String[] values = { studentId, studentId };
-		String[] columns = { "TOTAL" };
+		String[] values = {studentId, studentId};
+		String[] columns = {"TOTAL"};
 		String documentId = new DAODocument(document).getId();
-		if (this.connection.select(query, values, columns)[0][0].equals("0")) {
+		String[][] results = this.connection.select(query, values, columns);
+		if (results != null && results[0][0].equals("0")) {
 			query = "INSERT INTO Asignacion (idPracticante, idProyecto, profesorCalificador, " +
-				"documentoAsignacion, estaActivo) " +
-				"VALUES (?, ?, ?, ?, 1)";
-			values = new String[]{
+				"documentoAsignacion, estaActivo) VALUES (?, ?, ?, ?, 1)";
+			values = new String[] {
 				studentId,
 				projectId,
 				professorId,
 				documentId
 			};
 			assigned = this.connection.sendQuery(query, values);
-		} else {
-			assigned = false;
 		}
 		return assigned;
 	}
@@ -54,24 +52,23 @@ public class DAOAssignment {
 	public boolean removeAssignment() {
 		assert this.assignment.getStudent().isComplete() :
 			"Student is incomplete: DAOAssignment.assignProject()";
-		boolean removed;
+		boolean removed = false;
 		String studentId = new DAOStudent(this.assignment.getStudent()).getId();
 		String query = "SELECT COUNT(idPracticante) AS TOTAL FROM Asignacion " +
 			"WHERE idPracticante = ? AND estaActivo = 1";
-		String[] values = { studentId };
-		String[] columns = { "TOTAL" };
-		if (this.connection.select(query, values, columns)[0][0].equals("1")) {
+		String[] values = {studentId};
+		String[] columns = {"TOTAL"};
+		String[][] results = this.connection.select(query, values, columns);
+		if (results != null && results[0][0].equals("1")) {
 			query = "UPDATE Asignacion SET estaActivo = 0 WHERE idPracticante = ?";
-			values = new String[]{ studentId };
+			values = new String[] {studentId};
 			removed = this.connection.sendQuery(query, values);
-		} else {
-			removed = false;
 		}
 		return removed;
 	}
 	
 	public static boolean saveRequest(Student student, Project project) {
-		boolean saved;
+		boolean saved = false;
 		DBConnection connection = new DBConnection();
 		String query = "SELECT COUNT(idMiembro) AS TOTAL FROM Solicitud " +
 			"WHERE idMiembro = ? AND idProyecto = ? AND estaActiva = 1";
@@ -79,13 +76,11 @@ public class DAOAssignment {
 			new DAOStudent(student).getId(),
 			new DAOProject(project).getId()
 		};
-		String[] columns = { "TOTAL" };
-		if (connection.select(query, values, columns)[0][0].equals("0")) {
-			query = "INSERT INTO Solicitud (idMiembro, idProyecto) " +
-				"VALUES (?, ?)";
+		String[] columns = {"TOTAL"};
+		String[][] results = connection.select(query, values, columns);
+		if (results != null && results[0][0].equals("0")) {
+			query = "INSERT INTO Solicitud (idMiembro, idProyecto) VALUES (?, ?)";
 			saved = connection.sendQuery(query, values);
-		} else {
-			saved = false;
 		}
 		return saved;
 	}
@@ -96,8 +91,8 @@ public class DAOAssignment {
 		String query = "SELECT nombre " +
 			"FROM Proyecto INNER JOIN Solicitud ON Proyecto.idProyecto = Solicitud.idProyecto " +
 			"WHERE Solicitud.idMiembro = ?";
-		String[] values = { new DAOStudent(student).getId() };
-		String[] columns = { "nombre" };
+		String[] values = {new DAOStudent(student).getId()};
+		String[] columns = {"nombre"};
 		String[][] responses = connection.select(query, values, columns);
 		projects = new Project[responses.length];
 		for (int i = 0; i < responses.length; i++) {
