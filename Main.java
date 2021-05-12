b09ecd0161057c7c59958ad7f55bea78bae49a59
LuisGerardoRendon <50636600+LuisGerardoRendon@users.noreply.github.com>
2021-05-11 23:36:38 -0500

    cambios

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ import View.MainController;
 
 public class Main {
 	public static void main(String[] args) {
-		MainController.hit("MenuProyecto", "MenuProyecto", MainController.Sizes.MID);
+		MainController.hit("Login", "Login", MainController.Sizes.SMALL);
 	}
 }
